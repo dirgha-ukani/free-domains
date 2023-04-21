c0ac5e30b9da001eb3fcd077604e3318530c631c
dirgha-ukani <dirgha.ukani@truestaz.com>
2023-04-21 17:00:01 +0530

    Add .is-an.app.js File

---
+++ domains/dirgha-ukani.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'dirgha-ukani',
+  owner: {
+    repo: 'https://github.com/dirgha-ukani/dirgha-ukani.github.io',
+    email: 'dirgha.ukani@truestaz.com ,
+  },
+  record: {
+    CNAME: 'dirgha-ukani.github.io',
+  },
+  proxy: false,
+})
